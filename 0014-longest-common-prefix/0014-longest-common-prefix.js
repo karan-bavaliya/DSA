6e8bd340f7c11d330e9d7227580e9297295d1dad
karan-bavaliya <karanbavaliya2004@gmail.com>
2026-07-14 23:09:32 +0530

    Time: 1 ms (59.8%), Space: 54.2 MB (55.11%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -4,8 +4,6 @@
  */
 var longestCommonPrefix = function(strs) {
     let prefix = "";
-
-    // Check each character of the first string
     for (let i = 0; i < strs[0].length; i++) {
         let char = strs[0][i];
         for (let j = 1; j < strs.length; j++) {
